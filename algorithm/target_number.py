6f6afa8e61c56b026423c5234325f0fa4a27ba43
Ikk_git <83528634+Siho02@users.noreply.github.com>
2021-11-25 19:02:12 +0900

    Add files via upload

---
+++ algorithm/target_number.py
@@ -0,0 +1,26 @@
+from collections import deque
+def solution(numbers, target):
+    answer = 0 
+    queue = deque()
+    n = len(numbers)
+
+    queue.append([numbers[0], 0])
+    queue.append([-1 * numbers[0], 0])
+    print(queue)
+    #queue = [[1, 0]]
+
+    while queue:
+        temp, idx = queue.popleft()
+        idx += 1
+        if idx < n:
+            queue.append([temp + numbers[idx], idx])
+            queue.append([temp - numbers[idx], idx])
+        
+        else:
+            if temp == target:
+                answer += 1
+    
+    return answer
+
+
+print(solution([1,2,3,3], 3))
